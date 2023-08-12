86a1348019c44bdc035e15b57b60d64965c06c71
sunny7657 <ksvetlana466@gmail.com>
2023-08-12 16:39:58 +0200

    update files

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector('.gallery');
+
+function createMarkup(arr) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </li>`
+    )
+    .join('');
+}
+
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+container.addEventListener('click', handlerSizeUp);
+
+function handlerSizeUp(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+
+  const currentProduct = event.target.closest('.gallery');
+  // Не зовсім розумію навіщо делегування на ul.gallery; як потім використати цю змінну currentProduct, якщо отримую data-source i alt через event.target
+
+  const currentLink = event.target.dataset.source;
+  const currentAlt = event.target.alt;
+
+  const instance = basicLightbox.create(
+    `<img src="${currentLink}" alt="${currentAlt}" width="800" height="600">`
+  );
+  instance.show();
+
+  container.addEventListener('keydown', handlerSizeDown);
+
+  function handlerSizeDown(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  }
+}
